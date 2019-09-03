9e04081dfe3f7647a42e2b3bf48db980ddc89b68
unknown <tzviseliger@gmail.com>
2019-09-03 00:47:27 -0400

    saving console

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-console.log("say hello if the console is working")
+console.log("Hello World")
 
 /** 2) A first working Express Server */
 
